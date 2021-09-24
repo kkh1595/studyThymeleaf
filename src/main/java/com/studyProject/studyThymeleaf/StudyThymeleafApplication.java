c67a528546a842d21bd6cf34522fe9482a2a4289
KeunHyeong <kkh1595@gmail.com>
2021-09-24 18:58:12 +0900

    init

---
+++ src/main/java/com/studyProject/studyThymeleaf/StudyThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.studyProject.studyThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyProject/studyThymeleaf/StudyThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyProject.studyThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
